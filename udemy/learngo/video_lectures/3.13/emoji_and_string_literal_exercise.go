bc7bdf088777979f0fbf71b03265c431043fa63a
Jason Scarano <jscarano@posteo.net>
2026-01-29 16:53:52 -0700

    add string literal with emoji exercise

---
+++ udemy/learngo/video_lectures/3.13/emoji_and_string_literal_exercise.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("I ❤️❤️ Bikes!")
+
+	fmt.Println(
+		`string literal here
+		characters will be concatenated
+		and appear in the output just as they
+		are typed here`)
+}
