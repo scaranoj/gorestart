a6903586f945aca0c27525c7ee0d13bbb1bf6b28
Jason Scarano <jscarano@posteo.net>
2026-07-27 09:03:08 -0600

    add race condition

---
+++ udemy/learngo/video_lectures/exercise_hands_on_exercise_race_condition/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var incrementer = 0
+
+const gs = 100
+
+var wg sync.WaitGroup
+
+func main() {
+	runtime.NumCPU()
+	runtime.NumGoroutine()
+	wg.Add(gs)
+	for i := 0; i < gs; i++ {
+		go func() {
+			v := incrementer
+			runtime.Gosched()
+			v++
+			incrementer = v
+			fmt.Println("incrementer", incrementer)
+			wg.Done()
+		}()
+		fmt.Println("Goroutines", runtime.NumGoroutine())
+
+	}
+	wg.Wait()
+	fmt.Println("Goroutines", runtime.NumGoroutine())
+	fmt.Println("end value incrementer", incrementer)
+}
